554174f641718bfd44859233c07da1c3ec70d41a
unknown <peetmhlanga@gmail.com>
2022-03-02 03:14:44 +0200

    setup the app component

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React,{Component} from 'react';
+import{
+  BrowserRouter as Router,
+  Routes, Route
+}from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesPage from './pages/ArticlesPage';
+import ArticlesList from './pages/ArticlesList';
+
+
 import './App.css';
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
-  );
+class App extends Component {
+  render (){
+    return(
+      <Router>
+        <div classname="App">
+          <Routes>
+            <Route exact path="/" element={<HomePage/>}  />
+          </Routes>
+        </div>
+      </Router>
+      
+    );
+
+  }
+    
 }
 
 export default App;
