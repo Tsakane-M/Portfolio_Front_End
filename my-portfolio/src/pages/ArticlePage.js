0bbb458e8b2da1575b284f7d80f5d471ae937bd1
unknown <peetmhlanga@gmail.com>
2022-03-08 18:14:46 +0200

    added articles from a separate file + rudimentary functionality for non exisiting article

--- my-portfolio/src/pages/ArticlePage.js
+++ my-portfolio/src/pages/ArticlePage.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import articleContent from './article-content';
+
 
 export const ArticlePage = () =>{
     const{name}=useParams();
+    const article = articleContent.find(article =>article.name === name );
+    
+    if(!article) return <h1>Article does not exist!</h1>
+    
     return (
     <>
-    <h1>This is an {name} article</h1>
-    <p>
-    article page paragraph
-    </p>
-    </>)
+    <h1>{article.name}</h1>
+    {article.content.map((paragraph,key) => {
+        return  <p key ={key}>{paragraph}</p>;
+        
+    })}
+    </>  
+    );
+
+    
 };
 
 export default ArticlePage;
