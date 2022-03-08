b374ddb5db764b1d8f8998e0b315d052f6ebfd18
unknown <peetmhlanga@gmail.com>
2022-03-08 18:07:13 +0200

    linked the articles to the articles list page

--- my-portfolio/src/pages/ArticlesList.js
+++ my-portfolio/src/pages/ArticlesList.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import articleContent from './article-content';
+import {Link} from 'react-router-dom';
 
-const ArticlesList = () =>(
-    <>
-    <h1>Articles</h1>
-    <p>
-    articles list page paragraph
-    </p>
-    </>
-)
+export const ArticlesList = () =>{
+    return(
+        <>
+        <h1>Articles</h1>
+        {articleContent.map((article,key) =>{
+            return(
+                
+                <Link key={key} to={`/article/${article.name}`}>
+                    <h3>{article.title}</h3>
+                </Link>
+        )}
+        )}
+        </>
+    );
+            };
 
 export default ArticlesList;
