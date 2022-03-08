ccf109ebf217d8e9272049631d9855ba8ded4f93
unknown <peetmhlanga@gmail.com>
2022-03-08 18:06:32 +0200

    included a related articles list at the end of each article page

--- my-portfolio/src/pages/ArticlePage.js
+++ my-portfolio/src/pages/ArticlePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import articleContent from './article-content';
-
+import ArticlesListComponent from  '../components/ArticlesListComponent';
 
 export const ArticlePage = () =>{
     const{name}=useParams();
     const article = articleContent.find(article =>article.name === name );
     
     if(!article) return <h1>Article does not exist!</h1>
-    
+    const otherArticles = articleContent.filter(article => article.name !== name );
     return (
     <>
     <h1>{article.name}</h1>
@@ -16,6 +16,7 @@ export const ArticlePage = () =>{
         return  <p key ={key}>{paragraph}</p>;
         
     })}
+    <ArticlesListComponent articles ={otherArticles}/>
     </>  
     );
 
