8f95a6e9173ff76bdaa21de7876ab5fa123ea927
unknown <peetmhlanga@gmail.com>
2022-03-08 18:33:43 +0200

    completed front end

--- my-portfolio/src/pages/AboutPage.js
+++ my-portfolio/src/pages/AboutPage.js
@@ -6,7 +6,20 @@ export const AboutPage = () =>{
         <>
         <h1>About Me</h1>
         <p>
-        about page paragraph
+        I am a young black male who comes from Zimbabwe. I started my studies at UCT as an Electrical Engineering student. With time I realised
+that I was not cut out to be an Electrical Engineer, instead, I enjoyed the few Computer Science courses we took in the first year of the degree. At that time
+I was fresh from high school and had never coded before but understanding the
+concepts came naturally to me. 
+        </p>
+        <p>
+        At the back of my mind, I was always jealous of the
+students studying Computer Science. I would occasionally watch Python tutorials
+on Youtube. A combined lack of passion for EE and financial problems plus my love
+for Computer Science made me decide to change to a B.Sc in Computer Science
+after 3 years of study in the EE degree. I am very passionate about what I do now,
+I believe coding was meant for me and I am very excited to pursue this journey.
+
+
         </p>
         
         </>
--- my-portfolio/src/pages/HomePage.js
+++ my-portfolio/src/pages/HomePage.js
@@ -5,8 +5,14 @@ export const HomePage = () =>{
     <>
     <h1>Hello, Welcome to my Portfolio!</h1>
     <p>
-    home page paragraph
+    Innovative Entry-Level Programmer and Internet Entrepreneur striving to make the
+world a more unified and connected place. A creative thinker, adept in software
+development and working with various data structures. I am available to start work
+immediately and my notice period is 1 day.
     </p>
+    
+    
+
     </>
     );
 }
